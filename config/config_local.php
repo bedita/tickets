df120eb7c6a685715ed71f7cec4b21b40ae78dd2
stefanorosanelli <stefano.rosanelli@gmail.com>
2010-11-26 11:33:29 +0000

    tickets statuses

--- config/config_local.php
+++ config/config_local.php
@@ -4,4 +4,16 @@ $config["ticketSeverity"] = array (
 	"critical", "major", "normal", "minor"
 );
 
+$config["ticketStatus"] = array (
+	"new" => "draft",
+	"assigned" => "on",
+	"analysis" => "on",
+	"development" => "on",
+	"test" => "on",
+	"resolved" => "off",
+	"unresolvable" => "off",
+	"obsolete" => "off",
+);
+
+
 ?>
