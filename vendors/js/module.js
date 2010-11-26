7006dbb0d10878640aed1b0ff6bc4f8bdff23646
qwerg <glob.andrea@gmail.com>
2010-11-26 15:27:55 +0000

    vendors e istruzioni per possibile read/unread feature

---
+++ vendors/js/module.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	/* Funzione read/unread
+	 * quando entro in un view/detail mi salva nel cookie "BE_visited" (o in sessione?) una stringa che si chiama <id>+<lastmodified>
+	 * quando vedo l'elenco, per ogni TR cerco se il suo <id>+<lastmodified> è presente nel cookie (o in sessione?)
+	 * se NON c'è aggiunge al TR lo status "unread"
+	 * 
+	 */
+	
+
+});
