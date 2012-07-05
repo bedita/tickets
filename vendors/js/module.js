d215bb1dfc05b670aae467b9a425553375b52b51
stefanorosanelli <stefano.rosanelli@gmail.com>
2012-07-05 13:23:27 +0000

    tickets: fix default open tabs in view

--- vendors/js/module.js
+++ vendors/js/module.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-
-	/* Funzione read/unread
-	 * quando entro in un view/detail mi salva nel cookie "BE_visited" (o in sessione?) una stringa che si chiama <id>+<lastmodified>
-	 * quando vedo l'elenco, per ogni TR cerco se il suo <id>+<lastmodified> è presente nel cookie (o in sessione?)
-	 * se NON c'è aggiunge al TR lo status "unread"
-	 * 
-	 */
-	openAtStart("#ticketfilter");
 	
 	// uncheck closed ticket status if 'hide closed tickets' is selected
 	$("#filterHideClosed").change(function() {
