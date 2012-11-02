0aa5e45dea6ca49523cd0665c9612b00cf392131
batopa <batopa@gmail.com>
2012-11-02 13:30:13 +0100

    remove trailing spaces from Ticket.php

--- models/ticket.php
+++ models/ticket.php
@@ -1,21 +1,21 @@
 <?php
 /*-----8<--------------------------------------------------------------------
- * 
+ *
  * BEdita - a semantic content management framework
- * 
+ *
  * Copyright 2008 ChannelWeb Srl, Chialab Srl
- * 
+ *
  * This file is part of BEdita: you can redistribute it and/or modify
- * it under the terms of the Affero GNU General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or 
+ * it under the terms of the Affero GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * BEdita is distributed WITHOUT ANY WARRANTY; without even the implied 
+ * BEdita is distributed WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the Affero GNU General Public License for more details.
- * You should have received a copy of the Affero GNU General Public License 
+ * You should have received a copy of the Affero GNU General Public License
  * version 3 along with BEdita (see LICENSE.AGPL).
  * If not, see <http://gnu.org/licenses/agpl-3.0.html>.
- * 
+ *
  *------------------------------------------------------------------->8-----
  */
 
@@ -25,17 +25,22 @@
  */
 class Ticket extends BEAppObjectModel {
 
-	public $searchFields = array("title" => 8 , "description" => 4, 
+	public $searchFields = array("title" => 8 , "description" => 4,
 		"ticket_status" => 6, "severity" => 6);
 
 	public $objectTypesGroups = array("related");
-	
-	public $actsAs = array();
 
-	protected $modelBindings = array( 
-			"detailed" =>  array("BEObject" => array("ObjectType", 
-														"UserCreated", 
-														"UserModified", 
+	public $actsAs = array(
+		'CompactResult' 		=> array("ReferenceObject"),
+		'SearchTextSave',
+		'ForeignDependenceSave' => array('BEObject'),
+		'DeleteObject' 			=> 'objects'
+	);
+
+	protected $modelBindings = array(
+			"detailed" =>  array("BEObject" => array("ObjectType",
+														"UserCreated",
+														"UserModified",
 														"ObjectProperty",
 														"RelatedObject",
 														"Annotation",
@@ -43,18 +48,18 @@ class Ticket extends BEAppObjectModel {
 														"User",
 														"Version" => array("User.realname", "User.userid")
 														)),
-			"default" => array("BEObject" => array("ObjectProperty", 
+			"default" => array("BEObject" => array("ObjectProperty",
 								"ObjectType", "Annotation",
 								"Category", "RelatedObject","User" )),
 
-			"minimum" => array("BEObject" => array("ObjectType"))		
+			"minimum" => array("BEObject" => array("ObjectType"))
 	);
-	
+
 	function beforeValidate() {
         $this->checkDate('exp_resolution_date');
 	}
-	
-	function beforeSave() {	
+
+	function beforeSave() {
 		if("off" === $this->data["Ticket"]["status"] && empty($this->data["Ticket"]["closed_date"]) ) {
 			$this->data["Ticket"]["closed_date"] = date("Y-m-d H:i:s");
 		} else if(!empty($this->data["Ticket"]["closed_date"])){
@@ -62,6 +67,6 @@ class Ticket extends BEAppObjectModel {
 		}
 		return true ;
 	}
-	
+
 }
 ?>
