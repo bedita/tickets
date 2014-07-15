3fc91525d235e75f9d8c00efd993cd4c4e06978e
stefanorosanelli <stefano.rosanelli@gmail.com>
2014-07-15 14:57:34 +0200

    #11 fix git gscript

--- vendors/git-post-receive-hook.sh
+++ vendors/git-post-receive-hook.sh
@@ -8,15 +8,13 @@
 #       url = http://bedita.example.com/tickets/noteHook
 #       user = bedita
 #       passwd = bedita
-#       log = /path/to/local/logfile
+#       repo = git-repo-name
 
 read oldrev newrev
 
 url=$(git config tickets.url)
 userid=$(git config tickets.user)
 passwd=$(git config tickets.passwd)
-# local log file path
-#logfile=$(git config tickets.log)
 # repo logic name (must be the same in bedita.cfg.php server side)
 repo=$(git config tickets.repo)
 
@@ -25,5 +23,5 @@ echo "oldrev: $oldrev - newrev: $newrev"
 # formatted commit data to send
 commitData=`git log --pretty=format:"%cn|%H|%s###" $oldrev..$newrev | tr "\n" " "`
 
-echo "repo=$repo - userid=$userid - passwd=$passwd - commit_data=$commitData - url=$url" >> $logfile
-curl --request POST --data-urlencode "repo=$repo" --data-urlencode "userid=$userid" --data-urlencode "passwd=$passwd" --data-urlencode "commit_data=$commitData" $url >> $logfile
+echo "repo=$repo - userid=$userid - passwd=$passwd - commit_data=$commitData - url=$url"
+curl --request POST --data-urlencode "repo=$repo" --data-urlencode "userid=$userid" --data-urlencode "passwd=$passwd" --data-urlencode "commit_data=$commitData" $url
