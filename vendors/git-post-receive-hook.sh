5337a2e3f26b1234425d02d26d56ce08981b1d8d
stefanorosanelli <stefano.rosanelli@gmail.com>
2014-07-15 16:20:00 +0200

    #11 - remove password echo

--- vendors/git-post-receive-hook.sh
+++ vendors/git-post-receive-hook.sh
@@ -2,7 +2,8 @@
 #
 # 1. Write in .git/config file:
 #    url of bedita instance, ending with /tickets/noteHook
-#    user/password of a BEdita user with grants on tickets module     
+#    user/password of a BEdita user with grants on tickets module
+#    repo name, use the same on BEdita server, avoid '.'   
 #
 # [tickets]
 #       url = http://bedita.example.com/tickets/noteHook
@@ -23,5 +24,5 @@ echo "oldrev: $oldrev - newrev: $newrev"
 # formatted commit data to send
 commitData=`git log --pretty=format:"%cn|%H|%s###" $oldrev..$newrev | tr "\n" " "`
 
-echo "repo=$repo - userid=$userid - passwd=$passwd - commit_data=$commitData - url=$url"
+echo "repo=$repo - userid=$userid - passwd=********* - commit_data=$commitData - url=$url"
 curl --request POST --data-urlencode "repo=$repo" --data-urlencode "userid=$userid" --data-urlencode "passwd=$passwd" --data-urlencode "commit_data=$commitData" $url
